6932f683c27ce0b4bcd49166be49f9112437b0bc
Shashwat Panwar <shashwatpanwar17@gmail.com>
2022-09-21 19:53:14 -0400

    c1

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,15 @@
 print('Hello World' + '\n' + 'Shashwat Panwar' + '\n' + '5 years at uoft')
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 #c3
 
 #c4
 =======
+=======
+
+>>>>>>> 8cede33 (c1)
 #c1
 <<<<<<< HEAD
 >>>>>>> f987a28 (c1)
